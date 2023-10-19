11b54b4d4222e4908acf924bf9146e3dd7ea1978
Srikavya Vasala <95865936+srikavya26@users.noreply.github.com>
2023-10-19 17:24:44 +0530

    ggplot basics.R

--- ggplot basics.R
+++ ggplot basics.R
@@ -4,5 +4,7 @@ library(ggplot2)
 library(palmerpenguins)
 data(penguins)
 View(penguins)
+# to create the plot
 ggplot(data = penguins) + geom_point(mapping = aes(x = flipper_length_mm, y = body_mass_g))
+# geom_point function to creates a scatter plot of points
 ggplot(data = penguins, mapping = aes(x = flipper_length_mm, y = body_mass_g)) +  geom_point()
